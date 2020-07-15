191e3185b5f7663009366169c409abe93bd845eb
Codringtoncoding <humphrey.codrington@gmail.com>
2020-07-15 11:32:59 +0100

    first push for bus board

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Busboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
